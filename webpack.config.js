17658218c20b61731c65e678efefca2446460221
John <john.a.daniswara@gmail.com>
2023-02-13 15:41:28 -0800

    Annotate webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    // Specific entry point Webpack should use
     entry: './src/index.js',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        // Auto-creates an HTML file (`index.html` by default) which attaches 
+        // the generated JS bundles with <script> tags.
         new HtmlWebpackPlugin({}),
+        // Copies files from `public/` to `dist/`, useful for manifest.json file
+        // required by Chrome for extensions.
         new CopyPlugin({
             patterns: [
                 { from: "public/" },
